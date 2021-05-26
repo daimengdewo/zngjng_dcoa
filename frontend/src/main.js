7f1c3316883cac64d3f00d7b4b258eda6fa26545
unknown <471377158@qq.com>
2021-05-26 11:32:23 +0800

    2021-05-26

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,32 +10,46 @@ import "default-passive-events";
 import store from "./store";
 import AES from "./AES";
 
-
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true;
 
 // 添加请求拦截器，在请求头中加token
 axios.interceptors.request.use(
   config => {
-    if (localStorage.getItem('Authorization')) {
-      config.headers.Authorization = localStorage.getItem('Authorization');
+    if (localStorage.getItem("Authorization")) {
+      config.headers.Authorization = localStorage.getItem("Authorization");
     }
- 
+
     return config;
   },
   error => {
-    console.log(error);
     return Promise.reject(error);
-    
-  });
+  }
+);
 
+// 全局捕捉请求错误
+axios.interceptors.response.use(
+  response => {
+    // Do something before response is sent
+    return response;
+  },
+  error => {
+    // Do something with response error
+    if (error.response.status == 401) {
+      let res_data = error.response.data;
+      if (res_data.ret == 3) {
+        router.push("/login");
+        store.dispatch("loginOut");
+        ElementUI.Message.error("token已过期，请重新登录");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 Vue.use(ElementUI);
 Vue.prototype.$AES = AES;
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
-
-
-
 
 /* eslint-disable no-new */
 new Vue({
