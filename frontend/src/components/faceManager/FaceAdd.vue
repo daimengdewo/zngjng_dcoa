1e2627a6b888bbb682a1a55221354566f5e5f6af
unknown <471377158@qq.com>
2021-07-07 16:47:12 +0800

    2021-07-07 16:47

--- frontend/src/components/faceManager/FaceAdd.vue
+++ frontend/src/components/faceManager/FaceAdd.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="faceadd">
-      <h2>新增人脸</h2>
-    <van-form @submit="submit">
+    <van-form class="faceform" @submit="submit">
+      <h2 style="margin: 0; padding: 10px 0; box-shadow: 0 8px 12px #ebedf0">
+        新增人脸
+      </h2>
       <van-field
+        class="faceform"
         type="number"
         :rules="[{ required: true, message: '请填写员工ID' }]"
         required
@@ -11,6 +14,7 @@
         v-model="people_id"
       />
       <van-field
+        class="faceform"
         type="text"
         required
         name="name"
@@ -18,13 +22,24 @@
         :rules="[{ required: true, message: '请填写员工姓名' }]"
         v-model="people_name"
       />
-      <van-field name="uploader" label="人脸图片上传" required>
+      <van-field
+        class="faceform"
+        name="uploader"
+        label="人脸图片上传"
+        required
+        :rules="[{ required: true, message: '请上传人脸照片' }]"
+      >
         <template #input>
-          <van-uploader v-model="face_photo" multiple :max-count="1" preview-size="200px" :preview-full-image="false" />
-              
+          <van-uploader
+            v-model="face_photo"
+            multiple
+            :max-count="1"
+            preview-size="200px"
+            :preview-full-image="false"
+          />
         </template>
       </van-field>
-      <div style="margin: 16px">
+      <div style="padding: 16px">
         <van-button round block type="info" native-type="submit"
           >提交</van-button
         >
@@ -44,9 +59,8 @@ export default {
     };
   },
   methods: {
-
     submit(val) {
-        console.log(this.face_photo[0].content);
+      console.log(this.face_photo[0].content);
     },
   },
 };
@@ -58,16 +72,18 @@ export default {
   width: @max;
   height: @max;
   text-align: center;
+  background-color: #f7f8fa;
 }
-.preview-cover {
-    position: absolute;
-    bottom: 0;
-    box-sizing: border-box;
-    width: 100%;
-    padding: 4px;
-    color: #fff;
-    font-size: 12px;
-    text-align: center;
-    background: rgba(0, 0, 0, 0.3);
-  }
+.faceform {
+  background-color: #fff;
+}
+</style>
+
+<style>
+#faceadd .van-uploader__upload {
+  border-radius: 12px;
+}
+#faceadd span{
+  font-size: 14px;
+}
 </style>
